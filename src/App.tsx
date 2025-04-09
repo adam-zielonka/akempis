24ae8b98f5fb6ddf1635eea1d5f05111180bd0de
Adam Zielonka <root@adamzielonka.pro>
2025-04-09 10:50:41 +0200

    Remove css class names

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import book from './book.json'
 
 function App() {
   return <div>
-    <h1>O NAŚLADOWANIU CHRYSTUSA</h1>
+    <p>O NAŚLADOWANIU CHRYSTUSA</p>
     <p>tłumaczenie: Anna Kamieńska</p>
     {book.map((item, index) => {
-    return <div key={index} className="book">
+    return <div key={index}>
       <h2>{item.title}</h2>
       <h2>{item.subtitle}</h2>
       <div>
         {item.chapters.map((chapter, index) => {
-          return <div key={index} className="chapter">
+          return <div key={index}>
             <h3>{chapter.title}</h3>
             <h4>{chapter.subtitle}</h4>
             {chapter.paragraf.map((paragraf, index) => {
-              return <p key={index} className="paragraf">
+              return <p key={index}>
                 {paragraf}
               </p>})}
           </div>
