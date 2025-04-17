b208bd89520a34996419513aa569e976655b7f0f
Adam Zielonka <root@adamzielonka.pro>
2025-04-17 22:13:36 +0200

    Prepare table with dates

--- src/db/chapters.ts
+++ src/db/chapters.ts
@@ -1,17 +1,50 @@
 import fullBook from "./book.json";
 
+const startDateText = "2025-04-23";
+const brakeDateText = "2025-06-24";
+const afterBreakDateText = "2025-10-01";
 const db = getDB();
 
 function getDB() {
   const chapters = [];
+  let date = new Date(startDateText);
+  const brakeDate = new Date(brakeDateText);
+
+  function calculateDay() {
+    const newDate = new Date(date);
+
+    date.setDate(date.getDate() + 1);
+    if (date.getDay() === 2) {
+      date.setDate(date.getDate() + 1);
+    }
+
+    if (date.getTime() > brakeDate.getTime() && date.getMonth() < 9) {
+      date = new Date(afterBreakDateText);
+    }
+
+    const nextDate = new Date(date);
+    nextDate.setDate(nextDate.getDate() + 1);
+
+    return {
+      date: newDate,
+      nextDate: nextDate,
+    };
+  }
 
   for (const book of fullBook) {
-    chapters.push(...book.chapters.map((chapter) => ({
-      ...chapter,
-      bookTitle: book.title,
-      bookSubtitle: book.subtitle,
-      slug: `${book.title.split(" ")[1]}-${chapter.title.split(" ")[1]}`,
-    })));
+    
+    chapters.push(...book.chapters.map((chapter) => {
+      const { date, nextDate } = calculateDay();
+
+      return {
+        ...chapter,
+        bookTitle: book.title,
+        bookSubtitle: book.subtitle,
+        slug: `${book.title.split(" ")[1]}-${chapter.title.split(" ")[1]}`,
+        date,
+        nextDate,
+      };
+    }));
   }
 
   return chapters;
